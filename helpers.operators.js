41443e7da098a23e5a88579e0bf58020f3c6a437
Morten Henriksen <mh@gi2.dk>
2014-03-26 08:47:14 +0100

    Shift to use pure UI NS

--- helpers.operators.js
+++ helpers.operators.js
@@ -16,77 +16,77 @@ if (typeof Helpers === 'undefined') {
     Helpers = {};
 }
 
-if (typeof Handlebars !== 'undefined') {
-    Handlebars.registerHelper('getLength', function (a) {
+if (typeof UI !== 'undefined') {
+    UI.registerHelper('getLength', function (a) {
       return a && a.length;
     });
 
-    Handlebars.registerHelper('isSelected', function (a, b) {
+    UI.registerHelper('isSelected', function (a, b) {
       return (a === b)?' selected': '';
     });
 
-    Handlebars.registerHelper('isChecked', function (a, b) {
+    UI.registerHelper('isChecked', function (a, b) {
       return (a === b)?' checked': '';
     });
 
-    Handlebars.registerHelper('cutString', function (str, len) {
+    UI.registerHelper('cutString', function (str, len) {
       return (str.length > len)?str.substr(0, Math.max(len-3, 0))+'...':str;
     });
 
-    Handlebars.registerHelper('$eq', function (a, b) {
+    UI.registerHelper('$eq', function (a, b) {
       return (a === b); //Only text, numbers, boolean - not array & objects
     });
 
-    Handlebars.registerHelper('$neq', function (a, b) {
+    UI.registerHelper('$neq', function (a, b) {
       return (a !== b); //Only text, numbers, boolean - not array & objects
     });
 
-    Handlebars.registerHelper('$in', function (a, b, c, d) {
+    UI.registerHelper('$in', function (a, b, c, d) {
       return ( a === b || a === c || a === d);
     });
 
-    Handlebars.registerHelper('$nin', function (a, b, c, d) {
+    UI.registerHelper('$nin', function (a, b, c, d) {
       return ( a !== b && a !== c && a !== d);
     });
 
-    Handlebars.registerHelper('$exists', function (a) {
+    UI.registerHelper('$exists', function (a) {
       return ( a !== undefined);
     });
 
-    Handlebars.registerHelper('$lt', function (a, b) {
+    UI.registerHelper('$lt', function (a, b) {
       return (a < b);
     });
 
-    Handlebars.registerHelper('$gt', function (a, b) {
+    UI.registerHelper('$gt', function (a, b) {
       return (a > b);
     });
 
-    Handlebars.registerHelper('$lte', function (a, b) {
+    UI.registerHelper('$lte', function (a, b) {
       return (a <= b);
     });
 
-    Handlebars.registerHelper('$gte', function (a, b) {
+    UI.registerHelper('$gte', function (a, b) {
       return (a >= b);
     });
 
 
-    Handlebars.registerHelper('$and', function (a, b) {
+    UI.registerHelper('$and', function (a, b) {
       return (a && b);
     });
 
-    Handlebars.registerHelper('$or', function (a, b) {
+    UI.registerHelper('$or', function (a, b) {
       return (a || b);
     });
 
-    Handlebars.registerHelper('$not', function (a) {
+    UI.registerHelper('$not', function (a) {
       return (!a);
     });
 
-    Handlebars.registerHelper('getText', function (text) { // Deprecating
+    UI.registerHelper('getText', function (text) { // Deprecating
       return Helpers.getText(text);
     });
 
-    // Handlebars.registerHelper('userRole', function ( /* arguments */) {
+    // UI.registerHelper('userRole', function ( /* arguments */) {
     //   var role = Session.get('currentRole');
     //   return _.any(arguments, function(value) { return (value == role); });
     // });
@@ -147,14 +147,14 @@ if (typeof Handlebars !== 'undefined') {
     };
 
     Helpers.removeScope = function(name) {
-      delete Handlebars._globalHelpers[name];
+      delete UI._globalHelpers[name];
       delete Helpers.superScope[name];
     };
     
     Helpers.addScope('Session', Session);
     Helpers.addScope('Meteor', Meteor);
 
-    Handlebars.registerHelper('$', function() {
+    UI.registerHelper('$', function() {
       return Helpers.superScope;
     });
 }
