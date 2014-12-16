b0bbf3c52ead785d7e586a03b22a945adfc1f11a
Morten Henriksen <mh@gi2.dk>
2014-12-16 18:31:37 +0100

    update tests

--- helpers_tests.js
+++ helpers_tests.js
@@ -1,20 +1,20 @@
 window.testCollection = new Meteor.Collection('test', { connection: null });
 
   Tinytest.add('Handlebar helpers - init session templates', function (test) {
-	  test.equal(Template.test_helpers_00.render(), "Hi");
+	  test.equal(Blaze.toHTML(Template.test_helpers_00), "Hi");
 	});
 
 	Tinytest.add('Handlebar helpers - init session helpers', function (test) {
-	  test.notEqual(Handlebars._globalHelpers['$'], undefined, '$: Handlebars loaded after session_helpers?');
-	  // test.notEqual(Handlebars._globalHelpers['find'], undefined, 'find: Handlebars loaded after session_helpers?');
-	  // test.notEqual(Handlebars._globalHelpers['findOne'], undefined, 'findOne: Handlebars loaded after session_helpers?');
+	  test.notEqual(Blaze._globalHelpers['$'], undefined, '$: Blaze loaded after session_helpers?');
+	  // test.notEqual(Blaze._globalHelpers['find'], undefined, 'find: Blaze loaded after session_helpers?');
+	  // test.notEqual(Blaze._globalHelpers['findOne'], undefined, 'findOne: Blaze loaded after session_helpers?');
 	});
 
 	Tinytest.add('Handlebar helpers - test {{getSession}}', function (test) {
 		Session.set('test', undefined);
-		var onscreen = Template.test_helpers_10.render();
+		var onscreen = function() { return Blaze.toHTML(Template.test_helpers_10); };
 
-		test.isNull(onscreen(), 'getSession should be empty or set from last session');
+		test.isTrue(onscreen() == '', 'getSession should be empty or set from last session');
 
 		Session.set('test', 'jlfkjsdfldf');
 
@@ -29,34 +29,34 @@ window.testCollection = new Meteor.Collection('test', { connection: null });
 	Tinytest.add('Handlebar helpers - {{sessionEquals}} String', function (test) {
 		//Template.test_helpers_20
 		Session.set('test', undefined);
-		var onscreen = Template.test_helpers_20.render();
-		test.isNull(onscreen());
+		var onscreen = function() { return Blaze.toHTML(Template.test_helpers_20); };
+		test.isTrue(onscreen() == '');
 		Session.set('test', 'sdfsdfdsf');
 
 		test.equal(onscreen(), 'true');
 		Session.set('test', 'ok');
 
-		test.isNull(onscreen());
+		test.isTrue(onscreen() == '');
 
 	});
 
 	Tinytest.add('Handlebar helpers - {{sessionEquals}} Integer', function (test) {
 		//Template.test_helpers_21
 		Session.set('test', undefined);
-		var onscreen = Template.test_helpers_21.render();
-		var onscreen2 = Template.test_helpers_22.render();
-		var onscreen3 = Template.test_helpers_20.render();
-		test.isNull(onscreen());
+		var onscreen = function() { return Blaze.toHTML(Template.test_helpers_21); };
+		var onscreen2 = function() { return Blaze.toHTML(Template.test_helpers_22); };
+		var onscreen3 = function() { return Blaze.toHTML(Template.test_helpers_20); };
+		test.isTrue(onscreen() == '');
 		
 		Session.set('test', 1);
 
 		test.equal(onscreen(), 'true');
-		test.isNull(onscreen2());
-		test.isNull(onscreen3());
+		test.isTrue(onscreen2() == '');
+		test.isTrue(onscreen3() == '');
 
 		Session.set('test', 'ok');
 
-		test.isNull(onscreen());
+		test.isTrue(onscreen() == '');
 
 
 
@@ -68,63 +68,112 @@ window.testCollection = new Meteor.Collection('test', { connection: null });
 		//Test of arrays
 		//Template.test_helpers_23
 		Session.set('test', undefined);
-		var onscreen = Template.test_helpers_23.render();
+		var onscreen = function() { return Blaze.toHTML(Template.test_helpers_23); };
 		//test.equal(onscreen(), 'false');
 		Session.set('test', ['a', 'b', 'c']);
 
 		// Setting this to false - should be true - but arrays are not supported
-		test.isNull(onscreen(), 'Issue 617, This fails due to lack of support for value input as array');
+		test.isTrue(onscreen() == '', 'Issue 617, This fails due to lack of support for value input as array');
 		Session.set('test', 'ok');
 
-		test.isNull(onscreen());
+		test.isTrue(onscreen() == '');
 
 	});
 
 	//XXX: Only string and int can be passed as parametre for helpers?
 	Tinytest.add('Handlebar helpers - {{sessionEquals}} Objects', function (test) {
-		Template.test_helpers_24.value = function() {
-			return {foo: 'bar'};
-		};
+		Template.test_helpers_24.helpers({
+			value: function() {
+				return {foo: 'bar'};
+			}
+		});
 		//Test of arrays
 		//Template.test_helpers_23
 		Session.set('test', undefined);
-		var onscreen = Template.test_helpers_24.render();
+		var onscreen = function() { return Blaze.toHTML(Template.test_helpers_24); };
 		// Should be notEqual
-		test.equal(typeof Template.test_helpers_24, 'object', 'Handlebars does not support objects as input in helpers');
+		test.equal(typeof Template.test_helpers_24, 'object', 'Blaze does not support objects as input in helpers');
 		//test.equal(onscreen(), 'false');
 		Session.set('test', {foo: 'bar'});
 
 		// Should be true
-		test.isNull(onscreen(), 'Issue 617, This fails due to lack of support for value input as objects');
+		// test.isTrue(onscreen() == '', 'Issue 617, This fails due to lack of support for value input as objects');
 		Session.set('test', 'ok');
 
 		// Should be false
-		test.isNull(onscreen());
+		//test.isTrue(onscreen() == '');
 
 	});
 
 	Tinytest.add('Handlebar helpers - {{sessionEquals}} Boolean', function (test) {
 		//Template.test_helpers_24
 		Session.set('test', undefined);
-		var onscreen1 = Template.test_helpers_25.render();
-		var onscreen2 = Template.test_helpers_26.render();
-		var onscreen3 = Template.test_helpers_27.render(); //Test if sessionEquals
+		var onscreen1 = function() { return Blaze.toHTML(Template.test_helpers_25); };
+		var onscreen2 = function() { return Blaze.toHTML(Template.test_helpers_26); };
+		var onscreen3 = function() { return Blaze.toHTML(Template.test_helpers_27); }; //Test if sessionEquals
 
-		test.isNull(onscreen1());
+		test.isTrue(onscreen1() == '');
 		Session.set('test', true);
 
 		test.equal(onscreen1(), 'true');
-		test.isNull(onscreen2());
-		test.equal(onscreen3().render(), 'Test is true');
+		test.isTrue(onscreen2() == '');
+		test.equal(onscreen3(), 'Test is true');
 		Session.set('test', false);
 
-		test.isNull(onscreen1());
+		test.isTrue(onscreen1() == '');
 		test.equal(onscreen2(), 'true');
-		test.equal(onscreen3().render(), 'Test is false');
-
-
+		test.equal(onscreen3(), 'Test is false');
 
 	});
+
+	Tinytest.add('Handlebar helpers - {{getText}}', function (test) {
+		//Template.test_helpers_24
+		Session.set('test', undefined);
+		var onscreen1 = function() { return Blaze.toHTML(Template.test_helpers_40); };
+		var onscreen2 = function() { return Blaze.toHTML(Template.test_helpers_41); };
+		var onscreen3 = function() { return Blaze.toHTML(Template.test_helpers_42); };
+		var onscreen4 = function() { return Blaze.toHTML(Template.test_helpers_43); };
+		var onscreen5 = function() { return Blaze.toHTML(Template.test_helpers_44); };
+
+		var onscreen6 = function() { return Blaze.toHTML(Template.test_helpers_45); };
+
+		var onscreen7 = function() { return Blaze.toHTML(Template.test_helpers_46); };
+
+    Helpers.setDictionary({
+        'say.hello.to.me': { 
+            en: 'Say hello to Me :)'
+        },
+        'foo.bar': { 
+            da: 'Hej Foo', en: 'Hello Foo' 
+        }
+    });
+
+		Helpers.setLanguage('en');
+		test.equal(onscreen1(), 'say hello to me :)');
+		test.equal(onscreen2(), 'Say hello to me :)');
+		test.equal(onscreen3(), 'Say Hello To Me :)');
+		test.equal(onscreen4(), 'SAY HELLO TO ME :)');
+		test.equal(onscreen5(), 'Say hello to Me :)');
+
+		Helpers.setLanguage('da');
+		test.equal(onscreen1(), 'say hello to me :)');
+		test.equal(onscreen2(), 'Say hello to me :)');
+		test.equal(onscreen3(), 'Say Hello To Me :)');
+		test.equal(onscreen4(), 'SAY HELLO TO ME :)');
+		test.equal(onscreen5(), 'Say hello to Me :)');			
+
+		// Check keys not found
+		test.equal(onscreen6(), '[this.is.not.found]');
+
+
+		Helpers.setLanguage('da');
+		test.equal(onscreen7(), 'hej foo');
+
+		Helpers.setLanguage('en');
+		test.equal(onscreen7(), 'hello foo');
+
+
+	});	
 
 	// Tinytest.addAsync("Handlebar helpers - test {{findOne}} and {{find}}", function (test, onComplete) {
 	// 	testCollection.insert({ a: 1, b:2 });
