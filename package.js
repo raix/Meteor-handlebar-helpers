f4f7f3f11f8ed75ed31652d881eda62ae0b70990
Morten Henriksen <mh@gi2.dk>
2014-12-17 20:41:54 +0100

    Bump to version 0.2.2

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   git: 'https://github.com/raix/Meteor-handlebar-helpers.git',
   name: 'raix:handlebar-helpers',
-  version: '0.2.1',
+  version: '0.2.2',
   summary: "Handlebar helpers"
 });
 
